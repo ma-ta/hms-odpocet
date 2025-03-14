2bd902f86ed8b25d1dc9be3834f59985b073af66
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-03-14 20:27:11 +0100

    -

--- HMS_Odpocet_1.3.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem  -------------------------------------------
-rem  SKRIPT SPUSTI APLIKACI V MODU GDI SKALOVANI
-rem  -------------------------------------------
-
-set __COMPAT_LAYER=GdiDPIScaling DPIUnaware
-start HMS_Odpocet_1.3.exe
